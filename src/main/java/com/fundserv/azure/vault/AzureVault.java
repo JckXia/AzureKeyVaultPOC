739c5d4c9be1e7a2128a9eb0fb3e82c60075859e
JckXia <ys3xia@uwaterloo.ca>
2021-10-12 13:28:45 -0400

    Saving progress

--- src/main/java/com/fundserv/azure/vault/AzureVault.java
+++ src/main/java/com/fundserv/azure/vault/AzureVault.java
@@ -1,7 +1,26 @@
 package com.fundserv.azure.vault;
 
+import com.azure.core.util.polling.SyncPoller;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+
+
 public class AzureVault {
+    
     public static void main(String[] args) {
-        System.out.println("Running tests!");
+        String keyVaultName = System.getenv("KEY_VAULT_NAME");
+        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+        System.out.println(keyVaultUri);
+
+        SecretClient secretClient = new SecretClientBuilder()
+                        .vaultUrl(keyVaultUri)
+                        .credential(new DefaultAzureCredentialBuilder().build())
+                        .buildClient();
+        KeyVaultSecret retrievedSecret = secretClient.getSecret("NewSecKey");
+        System.out.println("Your secret is  " + retrievedSecret.getValue());
     }
 }
